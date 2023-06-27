9e56bf65f35637aeff548b98f2c6a2ddd216d41e
Morrofedd <76617504+Morrofedd@users.noreply.github.com>
2023-06-27 23:00:12 +0300

    Services primitive init

--- WpfApp1/Model/Coach.cs
+++ WpfApp1/Model/Coach.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WpfApp1.Model
 {
-    internal class Coach
+    internal class Coach//тренер
     {
         double mimir;
     }
--- WpfApp1/Model/Services.cs
+++ WpfApp1/Model/Services.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WpfApp1.Model
 {
-    internal class Services
+    internal class Services//услуги
     {
     }
 }
