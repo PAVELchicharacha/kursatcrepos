0986dd094784356bdc9c94381821aeb7f6473a27
Morrofedd <76617504+Morrofedd@users.noreply.github.com>
2023-06-29 19:31:12 +0300

    базовый вывод запроса

--- WpfApp1/View/WindowRequests.xaml.cs
+++ WpfApp1/View/WindowRequests.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using WpfApp1.ViewModel;
 
 namespace WpfApp1.View
 {
@@ -10,6 +11,22 @@ namespace WpfApp1.View
         public WindowRequests()
         {
             InitializeComponent();
+        }
+
+        private void Selector_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            switch (Selector.SelectedIndex)
+            {
+                case 0:
+                    DisplaceInformation.ItemsSource = new ClubVisitorVM().getVisitors();
+                    break;
+                case 1:
+                    DisplaceInformation.ItemsSource = new CoachVM().getCoaches();
+                    break;
+                    case 2:
+                    DisplaceInformation.ItemsSource = new ClubServiceVM().getServices();
+                    break;
+            }
         }
     }
 }
