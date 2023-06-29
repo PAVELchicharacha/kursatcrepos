8a8bb6e465657b973b55057996dab66db4d28d35
Morrofedd <76617504+Morrofedd@users.noreply.github.com>
2023-06-29 16:05:49 +0300

    Update ViewModel

--- WpfApp1/ViewModel/CoachViewmodel.cs
+++ WpfApp1/ViewModel/CoachViewmodel.cs
@@ -1,22 +1,26 @@
 ﻿using Microsoft.Data.Sqlite;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using WpfApp1.Model;
+using WpfApp1.View;
 
 namespace WpfApp1.ViewModel
 {
     internal class CoachViewmodel
     {
-        private List<Coach> coaches = new List<Coach>();
+        private List<Model.Coach> coaches = new List<Model.Coach>();
+        private List<Visitor> visitors = new List<Visitor>();
         private List<Services> services = new List<Services>();
 
         public CoachViewmodel()
         {
             ServicesFromDB();
             AddCoachFromDB();
+            AddVisitorsFromDB();
         }
 
-        void ServicesFromDB()
+        private void ServicesFromDB()
         {
             using (var connection = new SqliteConnection("Data Source=normalBAse.db"))
             {
@@ -43,10 +47,44 @@ namespace WpfApp1.ViewModel
             }
         }
 
-        public void addCoache(Coach coach)
+        public void addCoache(Model.Coach coach)
         {
             coaches.Add(coach);
+            using (var connection = new SqliteConnection("Data Source=normalBAse.db"))
+            {
+                connection.Open();
+
+                SqliteCommand command = new SqliteCommand();
+                command.Connection = connection;
+                command.CommandText = $"INSERT INTO coach (Name, SecondName, Age, Gender, Service) VALUES ('{coach.Name}', '{coach.SecondName}','{coach.Age}','{coach.Sex}','{coach.Service.Name}')";
+                command.ExecuteNonQuery();
+            }
         }
+        public void addService(Services service)
+        {
+            services.Add(service);
+            using (var connection = new SqliteConnection("Data Source=normalBAse.db"))
+            {
+                connection.Open();
+                SqliteCommand command = new SqliteCommand();
+                command.Connection = connection;
+                command.CommandText = $"INSERT INTO service (Coast, ServiceName, ServicePaymantType) VALUES ('{service.Price}', '{service.Name}','{service.PaymentType}')";
+                command.ExecuteNonQuery();
+            }
+        }
+        public void addVisitor(Visitor visitor)
+        {
+            visitors.Add(visitor);
+            using (var connection = new SqliteConnection("Data Source=normalBAse.db"))
+            {
+                connection.Open();
+                SqliteCommand command = new SqliteCommand();
+                command.Connection = connection;
+                command.CommandText = $"INSERT INTO visitors (Name, SecondName, Age, Gender, Service) VALUES ('{visitor.Name}', '{visitor.SecondName}','{visitor.Age}','{visitor.Sex}','{visitor.Service.Name}')";
+                command.ExecuteNonQuery();
+            }
+        }
+
 
         public void AddCoachFromDB()
         {
@@ -76,6 +114,34 @@ namespace WpfApp1.ViewModel
 
             }
         }
+        public void AddVisitorsFromDB()
+        {
+            using (var connection = new SqliteConnection("Data Source=normalBAse.db"))
+            {
+                connection.Open();
+                SqliteCommand command = connection.CreateCommand();
+                command.Connection = connection;
+
+                command.CommandText = "SELECT * FROM visitors";
+                using (SqliteDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows) // если есть данные
+                    {
+                        while (reader.Read())
+                        {
+                            visitors.Add(new Visitor(
+                                reader.GetValue(1).ToString(),
+                                reader.GetValue(2).ToString(),
+                                Convert.ToByte(reader.GetValue(3)),
+                                (Sex)Convert.ToByte(reader.GetValue(4)),
+                                GetServices(reader.GetValue(5).ToString())
+                                ));
+                        }
+                    }
+                }
+
+            }
+        }
 
         private Services GetServices(string Service)
         {
@@ -92,8 +158,9 @@ namespace WpfApp1.ViewModel
             return ser;
         }
 
-        public List<Coach> getCoaches() { return coaches; }
+        public List<Model.Coach> getCoaches() { return coaches; }
         public List<Services> getServices() { return services; }
+        public List<Visitor> getVisitors() {  return visitors; }
 
     }
 }
